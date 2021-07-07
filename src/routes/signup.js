a498a2b0482f9356f9258bb114c2df2bd1944792
Hyoukjoo <jo920208@gmail.com>
2021-07-07 16:07:17 +0900

    ✨ Add /signup/terms route

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,12 +1,11 @@
-
 let express = require("express");
 let signup = require("../controllers/signup");
 let router = express.Router();
-
 
 router.get("/phone", signup.phone);
 
 router.get("/userInfo", signup.userInfo);
 
+router.get("/terms", signup.terms);
 
 module.exports = router;
