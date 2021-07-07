eacafeea1562e0d75658d6e11d40a3a84f7866f9
황병현 <>
2021-07-08 00:14:05 +0900

    Change signup route and controller

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,19 +1,40 @@
+let db = require('../utils/nedb');
+let crypto = require('../utils/crypto')
+
 let signup = {
-  phone: (req, res) => {
+
+  registerView: (req, res) => {
     try {
-      res.render("phone");
+      res.render("register");
     } catch (error) {
-      res.render("phone");
+      res.render("register");
     }
   },
 
-  register: (req, res) => {
-    try {
-      res.render("register");
-    } catch (error) {
-      res.render("register");
+
+  register: async (req, res) => {
+    let params = req.body
+    const { password, salt } = await crypto(params.pwd);
+
+    let userData = {
+      ...params
     }
+    userData.pwd = password;
+    userData.salt = salt;
+
+
+    db.insert(userData, (err, row) => {
+      if (err !== null) {
+        console.log(err);
+        return;
+      }
+      res.json({
+        status: 200,
+      })
+    });
   },
+
+
   terms: (req, res) => {
     try {
       res.render("terms");
--- src/routes/signup.js
+++ src/routes/signup.js
@@ -2,9 +2,10 @@ let express = require("express");
 let signup = require("../controllers/signup");
 let router = express.Router();
 
-router.get("/phone", signup.phone);
 
-router.get("/register", signup.register);
+router.get("/register", signup.registerView);
+
+router.post("/register", signup.register);
 
 router.get("/terms", signup.terms);
 
