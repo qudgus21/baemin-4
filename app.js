e229119e10f97659de0dad19e691d95029a8ab10
황병현 <>
2021-07-08 16:48:58 +0900

    change port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-const serverPort = 3000;
+const serverPort = 80;
 const app = express();
 
 app.set("view engine", "pug");
