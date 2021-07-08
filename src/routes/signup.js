37d5eaac19be88b2ce4d0ebda8785f82f0944cd1
Hyoukjoo <jo920208@gmail.com>
2021-07-08 19:41:22 +0900

    ✨ Add signup router - post(/terms)

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -2,11 +2,12 @@ let express = require("express");
 let signup = require("../controllers/signup");
 let router = express.Router();
 
-
 router.get("/register", signup.registerView);
 
 router.post("/register", signup.register);
 
 router.get("/terms", signup.terms);
+
+router.post("/terms", signup.checkTerms);
 
 module.exports = router;
