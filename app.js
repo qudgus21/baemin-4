799352df59a64205a66523c6ddbd3b70351f9f81
황병현 <>
2021-07-08 17:12:24 +0900

    add env port

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-const serverPort = 80;
+const serverPort = process.env.PORT || 80;
+
 const app = express();
 
 app.set("view engine", "pug");
