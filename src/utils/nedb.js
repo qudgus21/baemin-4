40cbcc9180fe6e9496b1085248ae2e872da60295
Hyoukjoo <jo920208@gmail.com>
2021-07-08 19:36:41 +0900

    🚚 Move db path

--- src/utils/nedb.js
+++ src/utils/nedb.js
@@ -1,7 +1,7 @@
 const Nedb = require("nedb");
 
 const db = new Nedb({
-  filename: "./database/user.db", // DB 파일의 위치 및 이름
+  filename: "database/user.db", // DB 파일의 위치 및 이름
   autoload: true,
 });
 
